151e8b3ec243630354f2cd73796167abe756685b
Nthingtech <67294308+Nthingtech@users.noreply.github.com>
2024-03-14 15:07:12 -0300

    first commit

---
+++ src/main/java/org/ads/entities/User.java
@@ -0,0 +1,13 @@
+package org.ads.entities;
+
+import org.ads.entities.valueobjects.Email;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private Email email;
+
+
+
+}
---
+++ src/main/java/org/ads/entities/valueobjects/Name.java
@@ -0,0 +1,5 @@
+package org.ads.entities.valueobjects;
+
+public class Name {
+
+}
